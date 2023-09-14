ff5dc328b7fd47e74e568d3341de5063700644a8
Kevslhm <kevsherma@gmail.com>
2023-09-13 22:00:44 -0600

    I added a minigame that consist of guessing a random number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int userGuess = 0;
+        while (magicNumber != userGuess)
+        {
+            Console.Write("What is your guess? ");
+            string strGuess = Console.ReadLine();
+            userGuess = int.Parse(strGuess);
+
+            if (userGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
     }
 }
