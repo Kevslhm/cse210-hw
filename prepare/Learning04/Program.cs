ec35ccb88e0284aa421dc9b605348e63db3b7dc1
Kevslhm <kevsherma@gmail.com>
2023-10-03 13:47:56 -0600

    Learning activity succesfuly completed.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        //Console.WriteLine(assignment.GetSummary());
+
+        /* MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList()); */
+
+        WritingAssignment writingAssignment = new WritingAssignment("Marry Waters", "European History", "The Causes of World War 2");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
